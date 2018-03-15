5d497505628538c1102252524e12693fd24dcc1d
Huge Nguyen <nthieu30101985@gmail.com>
2018-03-15 17:28:46 +0700

    add config

---
+++ config.js
@@ -0,0 +1,10 @@
+exports.PORT = 3001;
+exports.DEBUG = true;
+exports.ENVIRONMENT = 'production';
+exports.CALLBACK_URL = 'http://localhost:3001';
+exports.PUSH_TOPIC = 'AccountsTopic';
+
+exports.CLIENT_ID = ""; // Consumer Key
+exports.CLIENT_SECRET = ""; // Consumer Secret
+exports.USERNAME = ""; // Salesforce Login Username
+exports.PASSWORD = "" // Salesforce Password + Token (i.e if abcd is the password and xxxxxx is the security token then this parameter would be: abcdxxxxxx)
