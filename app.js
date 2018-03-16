3d5fd4dd7474c4c1468a30b5f5677df44ba1b839
Huge Nguyen <nthieu30101985@gmail.com>
2018-03-16 09:15:23 +0700

    update and clean code

--- app.js
+++ app.js
@@ -7,14 +7,6 @@ var server = require('http').Server(app);
 app.set('port', process.env.PORT || 3001);
 app.use('/', routes);
 
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
 server.listen(app.get('port'), function () {
   console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
 });
